ef9b0ab5847e4412a4c5ea01bc31736aef222af3
Leszequi <78927384+Leszequi@users.noreply.github.com>
2021-11-28 18:58:47 +0100

    Res settings, cleanup

--- WaterIsAllICanSee/Assets/WaterIsAllICanSee/Scripts/SettingsMenu.cs
+++ WaterIsAllICanSee/Assets/WaterIsAllICanSee/Scripts/SettingsMenu.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+        }
+
+        resolutionDropdown.AddOptions(options);
+    }
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
